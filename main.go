d1f4c7e80d4fdb066b477e9b607dcbecae12fab0
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-05-09 07:10:15 +0500

    new project slack bot for uploading file

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
-	fmt.Println("hello go")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-2422907983831-3518453594352-EdgenDMQ6oYb51RIMm56yPBD")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03EX91K39P-3494671667411-b661fe4b68b28c0c1394d0ff4c34e1f2ba40cacaaf5a2d2754bde0a7fa1e80df")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("<param>", &slacker.CommandDefinition{
+		Description: "description",
+		Example:     "example",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
