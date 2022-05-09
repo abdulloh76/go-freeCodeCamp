4760534bf959f6b12bf297b34c23d140321823cd
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-05-09 10:59:08 +0500

    implement bot for uploading file to the channel

--- main.go
+++ main.go
@@ -1,48 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/shomali11/slacker"
+	"github.com/slack-go/slack"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-	for event := range analyticsChannel {
-		fmt.Println("Command Events")
-		fmt.Println(event.Timestamp)
-		fmt.Println(event.Command)
-		fmt.Println(event.Parameters)
-		fmt.Println(event.Event)
-		fmt.Println()
-	}
-}
-
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-2422907983831-3518453594352-EdgenDMQ6oYb51RIMm56yPBD")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03EX91K39P-3494671667411-b661fe4b68b28c0c1394d0ff4c34e1f2ba40cacaaf5a2d2754bde0a7fa1e80df")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-2422907983831-3492811706341-jTiUihroGhuSlXZdqa5RcemL")
+	os.Setenv("CHANNEL_ID", "C02D87DBD4Z")
 
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"linkedincv.pdf"}
 
-	go printCommandEvents(bot.CommandEvents())
-
-	bot.Command("<param>", &slacker.CommandDefinition{
-		Description: "description",
-		Example:     "example",
-		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-
-		},
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	err := bot.Listen(ctx)
-
-	if err != nil {
-		log.Fatal(err)
+	for _, file := range fileArr {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     file,
+		}
+		uploadedFile, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", uploadedFile.Name, uploadedFile.URL)
 	}
-
 }
