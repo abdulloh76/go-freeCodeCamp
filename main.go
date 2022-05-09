116564a1cd82c999287a3178513d7abb29c43857
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-05-09 07:03:50 +0500

    age calculating command handler

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/shomali11/slacker"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {}
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-2422907983831-3518453594352-EdgenDMQ6oYb51RIMm56yPBD")
@@ -15,4 +29,30 @@ func main() {
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my year of born is <year>", &slacker.CommandDefinition{
+		Description: "year of born calculator",
+		Example:     "my year of born is 2000",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error: year is invalid")
+			}
+			actualYear, _, _ := time.Now().Date()
+			age := actualYear - yob
+			res := fmt.Sprintf("age is %d", age)
+			response.Reply(res)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
